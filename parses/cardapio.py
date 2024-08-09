393d89baa9ba3b593025d590b0058f9b27b85cf7
Fabio Gutemberg <frostbitebr88@gmail.com>
2024-08-09 14:07:17 -0300

    feat: Exercício "Cardapio.py" Finalizado

--- parses/cardapio.py
+++ parses/cardapio.py
@@ -3,54 +3,60 @@ from xml.dom.minidom import parse
 dom = parse("xsd/cardapio.xml")
 cardapio = dom.documentElement
 pratos = cardapio.getElementsByTagName("prato")
+namespace = "http://cardapio.org"
 
-while (True):
-    print("\n" + "="*15 + " Cardapio " + "="*15 + "\n")
+while True:
+    print("\n" + "="*15 + " Cardápio " + "="*15 + "\n")
     for prato in pratos:
         prato_id = prato.getAttribute("id")
-        prato_nome = prato.getElementsByTagName("nome") [0]
+        prato_nome = prato.getElementsByTagName("nome")[0]
         nome = prato_nome.firstChild.nodeValue
         print(f"{prato_id}. {nome}")
     print("0. Finalizar")
 
-    entrada = input("\n Digite uma opção: ")
+    entrada = input("\nDigite uma opção: ")
     if entrada == "0":
         break
 
+    prato_selecionado = None
     for prato in pratos:
         prato_id = prato.getAttribute("id")
         if entrada == prato_id:
-            print("")
-            prato_nome = prato.getElementsByTagName("nome")[0]
-            nome = prato_nome.firstChild.nodeValue
-            print("="*15 + f" {nome} " + "="*15 + "\n")
+            prato_selecionado = prato
+            break
 
-            prato_description = prato.getElementsByTagName("descricao")[0]
-            description = prato_description.firstChild.nodeValue
-            print(f"{description} \n")
+    if prato_selecionado is None:
+        print("\nOpção inválida. Tente novamente.")
+        continue
 
-            ingredientes = prato.getElementsByTagName("ingrediente")
-            print("Ingredientes")
+    prato_nome = prato_selecionado.getElementsByTagName("nome")[0]
+    nome = prato_nome.firstChild.nodeValue
+    print("\n" + "="*15 + f" {nome} " + "="*15 + "\n")
 
-            numbering = 1
-            for ingrediente in ingredientes:
-                ingrediente_nome = ingrediente.firstChild.nodeValue
-                print(f" {numbering}. {ingrediente_nome} ")
-                numbering += 1
-            print("")
+    prato_descricao = prato_selecionado.getElementsByTagName("descricao")
+    descricao = prato_descricao[0].firstChild.nodeValue if prato_descricao else "Descrição não disponível"
+    print(f"{descricao} \n")
 
-            prato_price = prato.getElementsByTagName("preco")[0]
-            price = prato_price.firstChild.nodeValue
-            print(f"Preço: {price}")
+    ingredientes = prato_selecionado.getElementsByTagName("ingrediente")
+    print("Ingredientes")
 
-            prato_ptime = prato.getElementsByTagName("tempoPreparo")[0]
-            ptime = prato_ptime.firstChild.nodeValue
-            print(f"Tempo de Preparo: {ptime}")
+    for i, ingrediente in enumerate(ingredientes, start=1):
+        ingrediente_nome = ingrediente.firstChild.nodeValue
+        print(f"{i}. {ingrediente_nome}")
 
-            prato_calories = prato.getElementsByTagName("calorias")[0]
-            calories = prato_calories.firstChild.nodeValue
-            print(f"Calorias: {calories}")
+    prato_preco = prato_selecionado.getElementsByTagName("preco")[0]
+    preco = prato_preco.firstChild.nodeValue
+    print(f"Preço: {preco}")
 
-            entrada = input("\nEnter. Voltar ao menu \n")
+    prato_calorias = prato_selecionado.getElementsByTagName("calorias")[0]
+    calorias = prato_calorias.firstChild.nodeValue
+    print(f"Calorias: {calorias}")
 
+    prato_tempo_preparo = prato_selecionado.getElementsByTagNameNS(namespace, "tempodepreparo")
+    if prato_tempo_preparo:
+        tempo_preparo = prato_tempo_preparo[0].firstChild.nodeValue
+        print(f"Tempo de Preparo: {tempo_preparo}")
+    else:
+        print("Tempo de Preparo: Informação não disponível")
 
+    input("\nEnter. Voltar ao menu\n")
